935de1933cf6cbc4ba5d7409194a689d0b6a3804
Max <maxirovik@gmail.com>
2021-10-19 16:28:53 +0300

    extrareducers

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -2,14 +2,23 @@ import {AppDispatch} from "../store";
 import axios from "axios";
 import {IUser} from "../../models/IUser";
 import {userSlice} from "./UserSlice";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
 
-export const fetchUsers = () => async(dispatch:AppDispatch) => {
-    try {
-        dispatch((userSlice.actions.usersFetching()))
-        const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-        dispatch(userSlice.actions.usersFetchingSuccess(response.data))
-    } catch (error:any ) {
-        dispatch(userSlice.actions.usersFetchingError(error.message))
+// export const fetchUsers = () => async(dispatch:AppDispatch) => {
+//     try {
+//         dispatch((userSlice.actions.usersFetching()))
+//         const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+//         dispatch(userSlice.actions.usersFetchingSuccess(response.data))
+//     } catch (error:any ) {
+//         dispatch(userSlice.actions.usersFetchingError(error.message))
+//     }
+// }
+
+export const fetchUsers = createAsyncThunk(
+    'user/fetchAll',
+    async (_,thunkAPI) => {
+        const response = await axios.get<IUser>('https://jsonplaceholder.typicode.com/users')
+        return response.data
     }
-}
+)
