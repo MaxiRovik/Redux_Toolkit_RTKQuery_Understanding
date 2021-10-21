dd3abb30593ff980003994e91d370e257fd5b272
Max <maxirovik@gmail.com>
2021-10-21 17:32:38 +0300

    post to json-server has been created'build.mutation, method POST'

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,20 +1,31 @@
 import React, {FC, useEffect, useState} from 'react';
 import {postAPI} from "../services/PostService";
 import PostItem from "./PostItem";
+import {IPost} from "../models/IPost";
 
 const PostContainer :FC = () => {
     const [limit, setLimit] = useState(1)
     // const {data,  isLoading} = postAPI.useFetchAllPostsQuery(20);
     const {data,  isLoading, error, refetch} = postAPI.endpoints.fetchAllPosts.useQuery(limit, /*{pollingInterval: 1000}*/)
+    const [createPost, {}] = postAPI.useCreatePostMutation()
 
     useEffect (() => {
         setTimeout(() => {setLimit(10)},3000)
     },[])
 
+    const handleCreate = async () => {
+        const title = prompt()
+        await createPost({title, body: title} as IPost)
+    }
+
     return (
         <div>
-            <button onClick = {()=>refetch()}> Refresh </button>
-
+            <button onClick = {()=>refetch()} style = {{background:"lightblue"}}>
+                Refresh
+            </button>
+            <button onClick = {handleCreate} style = {{background:"lightgreen"}}>
+                Add new post
+            </button>
             {isLoading  && <h1>Loading... </h1>}
             {error  && <h1>error</h1>}
             <div className = "post__list">
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -4,17 +4,26 @@ import {IPost} from "../models/IPost";
 
 export const postAPI = createApi({
     reducerPath: 'postAPI',
-    baseQuery: fetchBaseQuery({baseUrl:
-            'http://localhost:4000'}),
-    endpoints: (build) =>({
+    baseQuery: fetchBaseQuery({
+        baseUrl:
+            'http://localhost:4000'
+    }),
+    endpoints: (build) => ({
         fetchAllPosts: build.query<IPost[], number>({
-            query: (limit=1) => ({
+            query: (limit = 1) => ({
                 url: '/posts',
-            params:{
-                  _limit: limit
-            }
+                params: {
+                    _limit: limit
+                }
             })
         }),
+        createPost: build.mutation<IPost, IPost>({
+            query: (post) => ({
+                url: '/posts',
+                method: "POST",
+                body: post
+            })
+        })
     })
 })
 
